3e39872f14d201aa06aa29005810fdd885bc6efb
superstar54 <xingwang1991@gmail.com>
2024-05-17 16:37:02 +0200

    increase timeout from 5 mins to 10 mins

--- deploy.sh
+++ deploy.sh
@@ -5,6 +5,7 @@ helm dependency build basehub 2> /dev/null || echo "No dependencies to update or
 # JupyterHub
 helm upgrade \
 	--install \
+	--timeout 600s \
 	--cleanup-on-fail \
 	--create-namespace --namespace=${K8S_NAMESPACE} \
 	${K8S_NAMESPACE} basehub
