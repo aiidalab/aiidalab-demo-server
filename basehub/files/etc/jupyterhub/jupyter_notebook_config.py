ed8e24cfcaab81754f2bd6776eceb05950461e03
Aliaksandr Yakutovich <yakutovicha@gmail.com>
2025-02-12 23:47:19 +0100

    Update cull settings in jupyter notebook config.

--- basehub/files/etc/jupyterhub/jupyter_notebook_config.py
+++ basehub/files/etc/jupyterhub/jupyter_notebook_config.py
@@ -2,11 +2,11 @@ c.NotebookApp.extra_template_paths.append('/etc/jupyterhub/templates')
 
 # cull_idle_timeout: timeout (in seconds) after which an idle kernel is
 # considered ready to be culled
-c.MappingKernelManager.cull_idle_timeout = 1200 # default: 0
+c.MappingKernelManager.cull_idle_timeout = 200 # default: 0
 
 # cull_interval: the interval (in seconds) on which to check for idle
 # kernels exceeding the cull timeout value
-c.MappingKernelManager.cull_interval = 120 # default: 300
+c.MappingKernelManager.cull_interval = 20 # default: 300
 
 # cull_connected: whether to consider culling kernels which have one
 # or more connections
