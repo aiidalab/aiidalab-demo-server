2d2b78565c8d846d2631bef748316b7d0bb96398
Edan Bainglass <45081142+edan-bainglass@users.noreply.github.com>
2025-04-02 16:57:46 +0200

    Discard policy templates (#34)
    These are now available in aiidalab-deployment-files

--- basehub/policy/generate.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-CONFIG=config.yaml
-OUTDIR=../files/etc/jupyterhub/templates
-
-jinja2 --format=yaml terms-of-use.j2 $CONFIG >$OUTDIR/terms-of-use.html
-jinja2 --format=yaml privacy-policy.j2 $CONFIG >$OUTDIR/privacy-policy.html
-
-export INCLUDE_POLICIES=True
